a8702c05de0bf2f35264e4d185246ad4aaebc8ab
Kraaven <dhruv@getinsightxr.com>
2024-02-28 23:40:17 +0530

    Created Replay
    -Created VR support
    
    
    Former-commit-id: 19ea3d9563b802696119c8b5606eae1e34404e76

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,12 +1,32 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
     private new Renderer renderer;
+    public GameObject ball;
+    
 
+    
     private void Start() => renderer = GetComponent<Renderer>();
 
     private void OnMouseEnter() => renderer.material.color = Color.red;
 
     private void OnMouseExit() => renderer.material.color = Color.white;
+
+    public void OnHover()
+    {
+        renderer.material.color = Color.red;
+    }
+
+    public void OnExit()
+    {
+        renderer.material.color = Color.white;
+    }
+
+    public void Shootball()
+    {
+        //Honestly, using a reference is better than doing get component....
+        ball.GetComponent<TargetSelector>().Shoot(transform.position);
+    }
 }

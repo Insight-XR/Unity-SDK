c8016ef66e9188489eb3fc097179f78fe0760297
shivam bhati <shivam.bhati@audifytech.com>
2024-03-03 16:31:50 +0530

    aws changes

--- Assets/Scripts/InsightXR/AmazonS3/AmazonS3.cs
+++ Assets/Scripts/InsightXR/AmazonS3/AmazonS3.cs
@@ -1,12 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AmazonS3 : MonoBehaviour
 {
-    public const string ACCESS_KEY  = "AKIAXQOF2EHXBET43ZUT";
-    public const string SECRET_KEY  = "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";
     public const string FOLDER_NAME = "test";
     public const string BUCKET_NAME = "shivam1807";
-
 }

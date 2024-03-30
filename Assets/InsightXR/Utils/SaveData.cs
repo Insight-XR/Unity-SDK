fa619cb929d800e0f07d6c6845ffd1220827afa1
Kraaven <161463369+DhruvInsight@users.noreply.github.com>
2024-03-30 16:31:18 +0530

    added basic SDK completion
    -Did Hands
    -Did Total SaveData class
    -Made it XR rig Compatible
    -Added Event Logging
    
    Whats Left:
    -Apply Overides
    -Remove half of the steps
    -Create New documentation (Editor Coroutines)
    -Code Clean Up
    
    
    Former-commit-id: d53736703fb736def18d24f97acfe7290c146e9f [formerly 75816d9d934214fd8e19998a3bb76b5d564a3c99]
    Former-commit-id: cd59c917ca8451ed10274d7f62529bff2771b841

--- Assets/InsightXR/Utils/SaveData.cs
+++ Assets/InsightXR/Utils/SaveData.cs
@@ -1,26 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using InsightXR.Network;
 using UltimateXR.Manipulation.HandPoses;
+using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SaveData
 {
     //Hand Pose Data
-    public List<(UxrHandDescriptor, UxrHandDescriptor)> handPoseData;
+    // public List<(UxrHandDescriptor, UxrHandDescriptor)> handPoseData;
+    
+    public string sessionID;
+    public string UserID;
+    public string CustomerID;
+    public string EndDateTime;
+    public string StartDateTime;
+    public string SessionDuration;
+
+    public List<(float, string)> EventLog;
+
+    public List<(float, float, float, float)> handPoseData;
 
     //The Object Motion data
     public Dictionary<string, List<ObjectData>> ObjectMotionData;
 
-    public string RecordingSaveTimeStamp;
+    
     
 
     //Initiation of the class
-    public SaveData(List<(UxrHandDescriptor, UxrHandDescriptor)> hand, Dictionary<string, List<ObjectData>> obj)
+    public SaveData(List<(float, float, float, float)> hand, Dictionary<string, List<ObjectData>> obj, DataHandleLayer controller)
     {
         handPoseData = hand;
         ObjectMotionData = obj;
-
-        RecordingSaveTimeStamp = System.DateTime.Now.ToString();
+        
+        if (controller != null)
+        {
+            sessionID = Random.Range(12345, 99999).ToString();
+            UserID = controller.UserID;
+            CustomerID = controller.CustomerID;
+            EndDateTime = System.DateTime.UtcNow.ToString("G");
+            StartDateTime = System.DateTime.UtcNow.Subtract(new TimeSpan(0, 0, (int)Time.time)).ToString("G");
+            SessionDuration = (Time.time * 1000).ToString("F2");
+            EventLog = controller.EventLog; 
+        }
+        
+        // RecordingSaveTimeStamp = System.DateTime.Now.ToString();
     }
 }

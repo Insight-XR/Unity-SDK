2923a424522db2ad4280dfc238537f7160205e94
Kraaven <dhruv@getinsightxr.com>
2024-04-21 15:17:52 +0530

    Added parent Logging also
    Former-commit-id: 2170ddcc39e1b87fa1061ff70661143cb7e9f8ea

--- Assets/InsightXR/Runtime/Utils/ObjectData.cs
+++ Assets/InsightXR/Runtime/Utils/ObjectData.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class ObjectData
 {
+    public string ParentObject;
     public float posx, posy, posz, rotx, roty, rotz, rotw;
 
-    public ObjectData(Vector3 pos, Quaternion rot)
+    public ObjectData(Vector3 pos, Quaternion rot, string Parent)
     {
+        ParentObject = Parent;
         (posx, posy, posz) = (pos.x,pos.y,pos.z);
         (rotx, roty, rotz, rotw) = (rot.x, rot.y, rot.z, rot.w);
         

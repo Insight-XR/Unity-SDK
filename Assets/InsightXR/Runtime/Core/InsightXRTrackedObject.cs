4ecf500b8c82add85bd9f1ef311c7ed7eb233de2
Kraaven <dhruv@getinsightxr.com>
2024-04-21 17:33:43 +0530

    fixed the parenting issue

--- Assets/InsightXR/Runtime/Core/InsightXRTrackedObject.cs
+++ Assets/InsightXR/Runtime/Core/InsightXRTrackedObject.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using InsightXR.Channels;
 using InsightXR.Network;
+using Newtonsoft.Json;
 using UnityEngine;
 
 namespace InsightXR.Core
@@ -16,36 +17,38 @@ namespace InsightXR.Core
         [SerializeField] private ComponentDataDistributionChannel DistributionChannel;
         
         
+        
 
         private void OnEnable() => DataCollectorWebMode.DistributionRequestEvent += MoveObject;
 
         private void OnDisable() => DataCollectorWebMode.DistributionRequestEvent -= MoveObject;
         
 
-        private void FixedUpdate() {
-            if (transform.parent != null)
-            {
-                DistributionChannel.RaiseEvent(name, new(transform.localPosition, transform.localRotation,transform.parent.name)); 
-            }
-            else
-            {
-                DistributionChannel.RaiseEvent(name, new(transform.localPosition, transform.localRotation,"World"));
-            }
-            
-        } 
+        // private void FixedUpdate() {
+        //
+        //     if (log)
+        //     {
+        //         Debug.Log(transform.parent.name);
+        //     }
+        //     if (transform.parent != null)
+        //     {
+        //         DistributionChannel.RaiseEvent(name, new(transform.localPosition, transform.localRotation,transform.parent.name)); 
+        //     }
+        //     else
+        //     {
+        //         DistributionChannel.RaiseEvent(name, new(transform.localPosition, transform.localRotation,"World"));
+        //     }
+        //     
+        // } 
 
         private void MoveObject(string name, ObjectData setToPoint)
         {
-            if (setToPoint.ParentObject == "World")
+            if (gameObject.name.Equals(name))
             {
-                transform.parent = null;
+                transform.parent = setToPoint.ParentObject == "World" ? null : GameObject.Find(setToPoint.ParentObject).transform;
+                transform.SetLocalPositionAndRotation(setToPoint.GetPosition(), setToPoint.GetRotation());
             }
-            else
-            {
-                transform.parent = GameObject.Find(setToPoint.ParentObject).transform;
-            }
-            if(gameObject.name.Equals(name)) transform.SetLocalPositionAndRotation(setToPoint.GetPosition(), setToPoint.GetRotation());
-            
+
         }
     }
 }

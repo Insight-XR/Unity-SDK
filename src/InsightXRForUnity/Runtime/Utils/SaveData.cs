c9024cca64c7b642d7b6a31ceb4df515902a9039
Akshat Khare <akshat.khare08@gmail.com>
2024-05-16 14:52:19 +0530

    remove visual scripting

--- src/InsightXRForUnity/Runtime/Utils/SaveData.cs
+++ src/InsightXRForUnity/Runtime/Utils/SaveData.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using InsightXR.Network;
-using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 

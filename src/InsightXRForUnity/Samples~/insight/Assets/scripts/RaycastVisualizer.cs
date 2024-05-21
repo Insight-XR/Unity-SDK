9dae2aa44d0dc57e09e2698a638b75335285f650
samarth0104 <144517774+samarth0104@users.noreply.github.com>
2024-05-21 12:36:06 +0530

    dino

--- src/InsightXRForUnity/Samples~/insight/Assets/scripts/RaycastVisualizer.cs
+++ src/InsightXRForUnity/Samples~/insight/Assets/scripts/RaycastVisualizer.cs
@@ -1,65 +1,59 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class QuadScript : MonoBehaviour
 {
-  private Dictionary<Material, List<Vector3>> materialHitPoints; // Store hit points for each material
+    private List<Vector3> mHitPoints;
+    private int mHitCount;
 
-  void Start()
-  {
-    materialHitPoints = new Dictionary<Material, List<Vector3>>();
-  }
-
-  void Update()
-  {
-    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-    RaycastHit hit;
-    Debug.DrawRay(ray.origin, ray.direction * 10, Color.green);
-
-    if (Physics.Raycast(ray, out hit, 100f, LayerMask.GetMask("HeatMapLayer")))
+    void Start()
     {
-      Debug.Log("Hit Object " + hit.collider.gameObject.name);
-      Debug.Log("Hit Texture coordinates = " + hit.textureCoord.x + "," + hit.textureCoord.y);
+        mHitPoints = new List<Vector3>(32);
+    }
 
-      // Get the MeshRenderer of the hit object
-      MeshRenderer hitRenderer = hit.collider.GetComponent<MeshRenderer>();
-      if (hitRenderer != null)
-      {
-        Material hitMaterial = hitRenderer.material;
-        Vector3 hitPoint = new Vector3(hit.textureCoord.x * 4 - 2, hit.textureCoord.y * 4 - 2, Random.Range(1f, 3f));
+    void Update()
+    {
+        // Create a ray from the camera's position, projecting forward
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        RaycastHit hit;
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.green);
 
-        // Add hit point to the list for this material
-        if (!materialHitPoints.ContainsKey(hitMaterial))
+        if (Physics.Raycast(ray, out hit, 100f, LayerMask.GetMask("HeatMapLayer")))
         {
-          materialHitPoints[hitMaterial] = new List<Vector3>(32);
+            Debug.Log("Hit Object " + hit.collider.gameObject.name);
+            Debug.Log("Hit Texture coordinates = " + hit.textureCoord.x + "," + hit.textureCoord.y);
+
+            // Get the MeshRenderer of the hit object
+            MeshRenderer hitRenderer = hit.collider.GetComponent<MeshRenderer>();
+            if (hitRenderer != null)
+            {
+                Vector3 hitPoint = new Vector3(hit.textureCoord.x * 4 - 2, hit.textureCoord.y * 4 - 2, Random.Range(1f, 3f));
+                mHitPoints.Add(hitPoint);
+
+                // Keep the list to a maximum of 32 points
+                if (mHitPoints.Count > 32)
+                {
+                    mHitPoints.RemoveAt(0); // Remove the oldest point to maintain a fixed size
+                }
+
+                UpdateMaterial(hitRenderer.material);
+            }
         }
-        materialHitPoints[hitMaterial].Add(hitPoint);
-
-        // Update the material with the cumulative hit points
-        UpdateMaterial(hitMaterial, materialHitPoints[hitMaterial]);
-      }
     }
-  }
 
-  private void UpdateMaterial(Material material, List<Vector3> hitPoints)
-  {
-    float[] pointsArray = new float[32 * 3];
-    int pointCount = Mathf.Min(hitPoints.Count, 32);
-    for (int i = 0; i < pointCount; i++)
+    private void UpdateMaterial(Material material)
     {
-      pointsArray[i * 3] = hitPoints[i].x;
-      pointsArray[i * 3 + 1] = hitPoints[i].y;
-      pointsArray[i * 3 + 2] = hitPoints[i].z;
-    }
+        float[] pointsArray = new float[32 * 3];
+        int pointCount = Mathf.Min(mHitPoints.Count, 32);
+        for (int i = 0; i < pointCount; i++)
+        {
+            pointsArray[i * 3] = mHitPoints[i].x;
+            pointsArray[i * 3 + 1] = mHitPoints[i].y;
+            pointsArray[i * 3 + 2] = mHitPoints[i].z;
+        }
 
-    material.SetFloatArray("_Hits", pointsArray);
-    material.SetInt("_HitCount", pointCount);
-
-    // If the list exceeds 32 points, remove the oldest ones
-    if (hitPoints.Count > 32)
-    {
-      hitPoints.RemoveRange(0, hitPoints.Count - 32);
+        material.SetFloatArray("_Hits", pointsArray);
+        material.SetInt("_HitCount", pointCount);
     }
-  }
 }
